f34748778f0be9b51b4d8bb4b5ae695bad6f53f2
Darragh Hayes <dara.hayes.1995@gmail.com>
2014-12-07 00:35:37 +0000

    fixed broken invalid sign in logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
           redirect_back_or user
         else
           flash[:error] = 'Invalid email/password combination'
-          redirect_ signin_path
+          redirect_to signin_path
         end
 	end
 
