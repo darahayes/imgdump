51801d25024f9151fa537d5a4512df25a0194d0e
Darragh Hayes <dara.hayes.1995@gmail.com>
2014-12-08 01:17:04 +0000

    fixed broken models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
-	has_many :votes, :as => :posts, dependent: :destroy
 	belongs_to :photo
 	belongs_to :user
 	default_scope order: 'comments.created_at DESC'
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,5 @@
 class Photo < ActiveRecord::Base
 	belongs_to :user
-	has_many :votes, :as => :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
 	accepts_nested_attributes_for :comments
 	mount_uploader :image, PhotoUploader
