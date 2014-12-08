5627bf70123db3ec8b645df0bca8292fe440960d
Darragh Hayes <dara.hayes.1995@gmail.com>
2014-12-08 00:59:22 +0000

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207221302) do
+ActiveRecord::Schema.define(version: 20141207125101) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -52,19 +52,5 @@ ActiveRecord::Schema.define(version: 20141207221302) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-
-  create_table "votes", force: true do |t|
-    t.boolean  "vote",          default: false, null: false
-    t.integer  "voteable_id",                   null: false
-    t.string   "voteable_type",                 null: false
-    t.integer  "voter_id"
-    t.string   "voter_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "votes", ["voteable_id", "voteable_type"], name: "index_votes_on_voteable_id_and_voteable_type"
-  add_index "votes", ["voter_id", "voter_type", "voteable_id", "voteable_type"], name: "fk_one_vote_per_user_per_entity", unique: true
-  add_index "votes", ["voter_id", "voter_type"], name: "index_votes_on_voter_id_and_voter_type"
 
 end
